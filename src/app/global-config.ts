c4467f64972545b858e6bc7eb1c738405a7c1a0c
aman <aman@tunerlabs.com>
2019-03-29 18:58:59 +0530

    resolved the conflicts

--- src/app/global-config.ts
+++ src/app/global-config.ts
@@ -1,83 +1,8 @@
 
 export const GlobalConfig = {
-<<<<<<< HEAD
-    acessAccordingRole : 'configurations/navigation',
-    currentPortal : 'program',
-    errorMessage:"Some thing went Wrong..",
-    programPortalLinks : [  
-        { 
-          linkHeading : "headings.features",
-          options:[
-            {
-              value:"headings.homes",
-              id:'home',
-              anchorLink:"home",
-              icon:"home"
-              
-            },
-            {
-              value :"headings.reports",
-              id : 'report',
-              anchorLink:"report",
-              icon:"description"
-            },
-                          {
-              value:"headings.operations",
-              id:'operations',
-              anchorLink:"operations",
-              icon:"build"
-            },
-          ]
-          }
-      ] ,
-    operationsDashBoardLinks :[
-        {
-          tooltip:"headings.operationDashboardUpload",
-          anchorLink:"/operations/upload-csv",
-          id:'uploadcsv',
-          icon:"settings",
-          disabled:false
-        },
-        {
-          tooltip:"headings.schoolListHeading",
-          anchorLink: "/operations/view-schools",
-          id:'viewSchools',
-          icon:"school",
-          disabled:true
-        },
-        {
-          tooltip:"headings.assessorListHeading",
-          anchorLink: "/operations/view-assessors",
-          id:'viewAcessor',
-          icon:"people",
-          disabled:false
-        },
-        {
-          icon:"description",
-          tooltip:"headings.opsReport",
-          anchorLink: "/operations/reports",
-          id:'opsReport',
-          disabled:true
-        }
-      ],
-      ReportDashBoardLinks : [
-        {
-          icons:"done",
-          tooltip:"headings.reportEntityReport",
-          anchorLink:"/report/entity-report",
-          id:'entityReport',
-          icon:"description",
-        },
-        {
-          icons:"done",
-          tooltip:"headings.schoolListHeading",
-          anchorLink:"/report/school-list",
-          id:'schoolList',
-          icon:"school",
-        }
-=======
   acessAccordingRole: 'configurations/navigation',
   currentPortal: 'program',
+  errorMessage: "Some thing went Wrong..",
   programPortalLinks: [
     {
       linkHeading: "headings.features",
@@ -93,7 +18,7 @@ export const GlobalConfig = {
           value: "headings.reports",
           id: 'report',
           anchorLink: "report",
-          icon: "graphic_eq"
+          icon: "description"
         },
         {
           value: "headings.operations",
@@ -101,7 +26,6 @@ export const GlobalConfig = {
           anchorLink: "operations",
           icon: "build"
         },
->>>>>>> made assessments as hide and lists in schools in program-portal
       ]
     }
   ],
@@ -110,25 +34,29 @@ export const GlobalConfig = {
       tooltip: "headings.operationDashboardUpload",
       anchorLink: "/operations/upload-csv",
       id: 'uploadcsv',
-      icon: "settings"
+      icon: "settings",
+      disabled: false
     },
     {
       tooltip: "headings.schoolListHeading",
       anchorLink: "/operations/view-schools",
       id: 'viewSchools',
-      icon: "school"
+      icon: "school",
+      disabled: true
     },
-    // {
-    //   tooltip: "headings.assessorListHeading",
-    //   anchorLink: "/operations/view-assessors",
-    //   id: 'viewAcessor',
-    //   icon: "people"
-    // },
+    {
+      tooltip: "headings.assessorListHeading",
+      anchorLink: "/operations/view-assessors",
+      id: 'viewAcessor',
+      icon: "people",
+      disabled: false
+    },
     {
       icon: "description",
       tooltip: "headings.opsReport",
       anchorLink: "/operations/reports",
-      id: 'opsReport'
+      id: 'opsReport',
+      disabled: true
     }
   ],
   ReportDashBoardLinks: [
@@ -148,3 +76,47 @@ export const GlobalConfig = {
     }
   ]
 }
+  // ],
+  // operationsDashBoardLinks: [
+  //   {
+  //     tooltip: "headings.operationDashboardUpload",
+  //     anchorLink: "/operations/upload-csv",
+  //     id: 'uploadcsv',
+  //     icon: "settings"
+  //   },
+  //   {
+  //     tooltip: "headings.schoolListHeading",
+  //     anchorLink: "/operations/view-schools",
+  //     id: 'viewSchools',
+  //     icon: "school"
+  //   },
+  //   // {
+  //   //   tooltip: "headings.assessorListHeading",
+  //   //   anchorLink: "/operations/view-assessors",
+  //   //   id: 'viewAcessor',
+  //   //   icon: "people"
+  //   // },
+  //   {
+  //     icon: "description",
+  //     tooltip: "headings.opsReport",
+  //     anchorLink: "/operations/reports",
+  //     id: 'opsReport'
+  //   }
+  // ],
+//   ReportDashBoardLinks: [
+//     {
+//       icons: "done",
+//       tooltip: "headings.reportEntityReport",
+//       anchorLink: "/report/entity-report",
+//       id: 'entityReport',
+//       icon: "description",
+//     },
+//     {
+//       icons: "done",
+//       tooltip: "headings.schoolListHeading",
+//       anchorLink: "/report/school-list",
+//       id: 'schoolList',
+//       icon: "school",
+//     }
+//   ]
+// }
