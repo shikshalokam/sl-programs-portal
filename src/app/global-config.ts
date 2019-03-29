553aaeca5884c13bd298857f776f1bf3b5e8450a
aman <aman@tunerlabs.com>
2019-03-29 11:00:04 +0530

    made assessments as hide and lists in schools in program-portal

--- src/app/global-config.ts
+++ src/app/global-config.ts
@@ -1,73 +1,73 @@
 
 export const GlobalConfig = {
-    acessAccordingRole : 'configurations/sideBar',
-    currentPortal : 'program',
-    programPortalLinks : [  
-        { 
-          linkHeading : "headings.features",
-          options:[
-            {
-              value:"headings.homes",
-              id:'home',
-              anchorLink:"home",
-              icon:"home"
-              
-            },
-            {
-              value :"headings.reports",
-              id : 'report',
-              anchorLink:"report",
-              icon:"description"
-            },
-                          {
-              value:"headings.operations",
-              id:'operations',
-              anchorLink:"operations",
-              icon:"build"
-            },
-          ]
-          }
-      ] ,
-    operationsDashBoardLinks :[
+  acessAccordingRole: 'configurations/navigation',
+  currentPortal: 'program',
+  programPortalLinks: [
+    {
+      linkHeading: "headings.features",
+      options: [
         {
-          tooltip:"headings.operationDashboardUpload",
-          anchorLink:"/operations/upload-csv",
-          id:'uploadcsv',
-          icon:"settings"
+          value: "headings.homes",
+          id: 'home',
+          anchorLink: "home",
+          icon: "home"
+
         },
         {
-          tooltip:"headings.schoolListHeading",
-          anchorLink: "/operations/view-schools",
-          id:'viewSchools',
-          icon:"school"
+          value: "headings.reports",
+          id: 'report',
+          anchorLink: "report",
+          icon: "graphic_eq"
         },
         {
-          tooltip:"headings.assessorListHeading",
-          anchorLink: "/operations/view-assessors",
-          id:'viewAcessor',
-          icon:"people"
+          value: "headings.operations",
+          id: 'operations',
+          anchorLink: "operations",
+          icon: "build"
         },
-        {
-          icon:"description",
-          tooltip:"headings.opsReport",
-          anchorLink: "/operations/reports",
-          id:'opsReport'
-        }
-      ],
-      ReportDashBoardLinks : [
-        {
-          icons:"done",
-          tooltip:"headings.reportEntityReport",
-          anchorLink:"/report/entity-report",
-          id:'entityReport',
-          icon:"description",
-        },
-        {
-          icons:"done",
-          tooltip:"headings.schoolListHeading",
-          anchorLink:"/report/school-list",
-          id:'schoolList',
-          icon:"school",
-        }
       ]
     }
+  ],
+  operationsDashBoardLinks: [
+    {
+      tooltip: "headings.operationDashboardUpload",
+      anchorLink: "/operations/upload-csv",
+      id: 'uploadcsv',
+      icon: "settings"
+    },
+    {
+      tooltip: "headings.schoolListHeading",
+      anchorLink: "/operations/view-schools",
+      id: 'viewSchools',
+      icon: "school"
+    },
+    // {
+    //   tooltip: "headings.assessorListHeading",
+    //   anchorLink: "/operations/view-assessors",
+    //   id: 'viewAcessor',
+    //   icon: "people"
+    // },
+    {
+      icon: "description",
+      tooltip: "headings.opsReport",
+      anchorLink: "/operations/reports",
+      id: 'opsReport'
+    }
+  ],
+  ReportDashBoardLinks: [
+    {
+      icons: "done",
+      tooltip: "headings.reportEntityReport",
+      anchorLink: "/report/entity-report",
+      id: 'entityReport',
+      icon: "description",
+    },
+    {
+      icons: "done",
+      tooltip: "headings.schoolListHeading",
+      anchorLink: "/report/school-list",
+      id: 'schoolList',
+      icon: "school",
+    }
+  ]
+}
