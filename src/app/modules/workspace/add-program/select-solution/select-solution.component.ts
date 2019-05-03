d1b075bbd6e16b0a5060770ff78054135b51c1ac
siba-tunerlabs <siba.sankar@tunerlabs.com>
2019-05-03 17:36:22 +0530

    redesign the solution listing with filters page

--- src/app/modules/workspace/add-program/select-solution/select-solution.component.ts
+++ src/app/modules/workspace/add-program/select-solution/select-solution.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { link } from 'fs';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { MatTableDataSource } from '@angular/material';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-select-solution',
   templateUrl: './select-solution.component.html',
-  styleUrls: ['./select-solution.component.scss']
+  styleUrls: ['./select-solution.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class SelectSolutionComponent implements OnInit {
   searchSolution = '';
@@ -31,18 +41,78 @@ export class SelectSolutionComponent implements OnInit {
       title: "Tibel",
       description:"Tibel solutions for adding in programs",
       icon: 'assignment_turned_in ',
+    }, {
+      title: "DCPCR",
+      description:"Dcpcr solutions for adding in programs",
+      icon: 'assignment_turned_in ',
+    },
+    {
+      title: "MYANTRA",
+      description:"Myantra solutions for adding in programs",
+      icon: 'assignment_turned_in ',
+    },
+    {
+      title: "TUNERLABS",
+      description:"Tunerlabs solutions for adding in programs",
+      icon: 'assignment_turned_in ',
+    },
+    {
+      title: "Tibel",
+      description:"Tibel solutions for adding in programs",
+      icon: 'assignment_turned_in ',
+    },
+    {
+      title: "DCPCR",
+      description:"Dcpcr solutions for adding in programs",
+      icon: 'assignment_turned_in ',
+    },
+    {
+      title: "MYANTRA",
+      description:"Myantra solutions for adding in programs",
+      icon: 'assignment_turned_in ',
+    },
+    {
+      title: "TUNERLABS",
+      description:"Tunerlabs solutions for adding in programs",
+      icon: 'assignment_turned_in ',
+    },
+    {
+      title: "Tibel",
+      description:"Tibel solutions for adding in programs",
+      icon: 'assignment_turned_in ',
+    }, {
+      title: "DCPCR",
+      description:"Dcpcr solutions for adding in programs",
+      icon: 'assignment_turned_in ',
+    },
+    {
+      title: "MYANTRA",
+      description:"Myantra solutions for adding in programs",
+      icon: 'assignment_turned_in ',
+    },
+    {
+      title: "TUNERLABS",
+      description:"Tunerlabs solutions for adding in programs",
+      icon: 'assignment_turned_in ',
+    },
+    {
+      title: "Tibel",
+      description:"Tibel solutions for adding in programs",
+      icon: 'assignment_turned_in ',
     }
   ]
   headings:"headings.selectSolution";
+  displayedColumns: string[] = ['select', 'title', 'description'];
+dataSource = new MatTableDataSource(this.solutions);
+selection = new SelectionModel(true, []);
+expandedElement;
   constructor(
     private router : Router
   ) { }
 
   ngOnInit() {
   }
-  applyFilter(event){
-    this.searchSolution = event;
-  }
+  
   searchInApi(event){
     console.log(event)
   }
@@ -51,15 +121,46 @@ export class SelectSolutionComponent implements OnInit {
     this.selectSolutionType = type;
   }
   onAddSolution(){
-    this.router.navigate(['/workspace/add-program']);
+    // this.router.navigate(['/workspace/add-program']);
+    console.log(this.selection.selected)
   }
   insertSolution(){
 
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
   solutionDetails(solution){
     let link ='/workspace/solution-details';
     this.router.navigate([]).then(result => {  window.open(link, '_blank'); });
 
   }
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+  
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+  
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  
+  }
+    toggleRow(row) {
+      console.log(row);
+      this.selection.toggle(row);
+
+    }
 }
+
